2a628ffed7a50e3f94920de44a8a8731cd262f7c
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2020-01-15 13:49:59 -0300

    code refactor

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,16 +1,8 @@
 <template>
   <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
+    <v-layout text-center wrap>
       <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
+        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200"></v-img>
       </v-flex>
 
       <v-flex mb-4>
@@ -24,58 +16,31 @@
         </p>
       </v-flex>
 
-      <v-flex
-        mb-5
-        xs12
-      >
+      <v-flex mb-5 xs12>
         <h2 class="headline font-weight-bold mb-3">What's next?</h2>
 
         <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
+          <a v-for="(next, i) in whatsNext" :key="i" :href="next.href" class="subheading mx-3" target="_blank">
             {{ next.text }}
           </a>
         </v-layout>
       </v-flex>
 
-      <v-flex
-        xs12
-        mb-5
-      >
+      <v-flex xs12 mb-5>
         <h2 class="headline font-weight-bold mb-3">Important Links</h2>
 
         <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
+          <a v-for="(link, i) in importantLinks" :key="i" :href="link.href" class="subheading mx-3" target="_blank">
             {{ link.text }}
           </a>
         </v-layout>
       </v-flex>
 
-      <v-flex
-        xs12
-        mb-5
-      >
+      <v-flex xs12 mb-5>
         <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
 
         <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
+          <a v-for="(eco, i) in ecosystem" :key="i" :href="eco.href" class="subheading mx-3" target="_blank">
             {{ eco.text }}
           </a>
         </v-layout>
@@ -86,59 +51,59 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
 
   data: () => ({
     ecosystem: [
       {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader"
       },
       {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify"
       },
       {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify"
+      }
     ],
     importantLinks: [
       {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
+        text: "Documentation",
+        href: "https://vuetifyjs.com"
       },
       {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
+        text: "Chat",
+        href: "https://community.vuetifyjs.com"
       },
       {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify"
       },
       {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs"
       },
       {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
+        text: "Articles",
+        href: "https://medium.com/vuetify"
+      }
     ],
     whatsNext: [
       {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer"
       },
       {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/layout/pre-defined"
       },
       {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
+      }
+    ]
+  })
 };
 </script>
