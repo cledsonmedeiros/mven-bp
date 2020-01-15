f19d1d5a1db04f1f80011ccd8bf3e2ea23a2da15
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2020-01-15 13:44:56 -0300

    basic vuetify

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <Navbar></Navbar>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import Navbar from '@/components/layout/Navbar.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar
   }
 }
 </script>
